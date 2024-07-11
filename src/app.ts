7e3d15137ad9d653ac48f43b8da16ea7ceffa903
tunaunnie <ygw021227@gmail.com>
2024-07-11 16:39:37 +0900

    first

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.post('/users', async (req, res) => {
+  const { email, name } = req.body;
+  const user = await prisma.user.create({
+    data: { email, name },
+  });
+  res.json(user);
+});
+
+export default app;
