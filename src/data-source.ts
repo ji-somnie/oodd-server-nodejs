76185879f4b11037858368178f91b2ef034e1840
haerxeong <haeryeong04@naver.com>
2024-08-02 16:35:45 +0900

    Feat: 좋아요 조회 기능 구현

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,7 +11,7 @@ export const myDataBase = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE, // 스키마 이름
-  entities: ['src/entity/*.ts'], // 모델의 경로
+  entities: ['src/entities/*.ts'], // 모델의 경로
   logging: true, // 정확히 어떤 sql 쿼리가 실행됐는지 로그 출력
   synchronize: false, // 현재 entity 와 실제 데이터베이스 상 모델을 동기화
 });
