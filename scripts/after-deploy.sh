5e69df9c9c6861132e614cd363b126cfe6e95ad8
Minseo Lim <limms1217@naver.com>
2024-08-06 14:36:00 +0900

    24.08.06 찐찐막 CI/CD

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 REPOSITORY=/home/ubuntu/build
+APP_NAME=node_app
 
 cd $REPOSITORY
 
 sudo yarn install
 
-sudo pm2 start dist/app.js --name node_app
+# Check if the app is already running
+if pm2 list | grep $APP_NAME > /dev/null
+then
+  echo "$APP_NAME is already running. Restarting..."
+  sudo pm2 restart $APP_NAME
+else
+  echo "Starting $APP_NAME"
+  sudo pm2 start dist/app.js --name $APP_NAME
+fi
