09ba1b7bcc212ba7e15669f5f760306f72ea2a3a
tunaunnie <ygw021227@gmail.com>
2024-08-08 13:39:31 +0900

    fix: type error

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,6 +1,5 @@
 import { Repository } from "typeorm";
 import { myDataBase } from "../data-source";
-import { myDataBase } from "../data-source";
 import { User } from "../entities/userEntity";
 
 export class UserRepository extends Repository<User> {
