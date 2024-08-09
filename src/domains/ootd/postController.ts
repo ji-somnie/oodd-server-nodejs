a787341a9bfcd614f8f88074e47a0a9bf2739b37
ji-somnie <ie8907@ewhain.net>
2024-08-09 16:53:19 +0900

    Fix: 스타일 태그 검색 결과 조회 로직 수정

--- src/domains/ootd/postController.ts
+++ src/domains/ootd/postController.ts
@@ -82,4 +82,20 @@ router.get('/:postId', async (req: Request, res: Response): Promise<void> => {
   }
 });
 
+// 스타일태그 검색 결과 조회 
+router.get('/tag/:styletag', async (req: Request, res: Response): Promise<void> => {
+  try {
+    const tag = req.params.styletag;
+
+    const getPostsByTagResponse = await postService.getPostsByTag(userId, tag);
+
+    if (getPostsByTagResponse.isSuccess) {
+      res.status(HTTP_OK.status).json(getPostsByTagResponse);
+    } 
+  } catch (error) {
+    console.error(error);
+    res.status(HTTP_INTERNAL_SERVER_ERROR.status).json({ message: HTTP_INTERNAL_SERVER_ERROR.message });
+  }
+});
+
 export default router;
