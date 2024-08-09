f3e3f30da736af3ecb1aa41e531e7b520cc16f64
배서연 <alicebsy@naver.com>
2024-08-09 12:31:58 +0900

    fix: 게시물 신고 기능 구현

--- src/entities/userEntity.ts
+++ src/entities/userEntity.ts
@@ -1,14 +1,32 @@
-import { Entity, Column } from "typeorm";
-import { BaseEntity } from "../base/baseEntity";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { NotificationEntity } from './notificationEntity';
+import { ReportEntity } from './reportEntity';
+import { MatchRequestEntity } from './matchRequestEntity'; // MatchRequestEntity import 추가
 
-@Entity()
-export class UserEntity extends BaseEntity {
+@Entity('users')
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
     @Column()
-    name: string;
+    name!: string;
 
     @Column()
-    email: string;
+    email!: string;
 
-    @Column({ default: "active" })
-    status: string;
+    @Column()
+    status!: string;
+
+    @OneToMany(() => NotificationEntity, notification => notification.user)
+    notifications!: NotificationEntity[];
+
+    @OneToMany(() => ReportEntity, report => report.reportedBy)
+    reports!: ReportEntity[];
+
+    @OneToMany(() => MatchRequestEntity, matchRequest => matchRequest.requester) // sentRequests 속성 추가
+    sentRequests!: MatchRequestEntity[];
+
+    @OneToMany(() => MatchRequestEntity, matchRequest => matchRequest.requestee) // receivedRequests 속성 추가
+    receivedRequests!: MatchRequestEntity[];
+
 }
