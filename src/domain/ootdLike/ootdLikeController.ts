e2be39791edb384346b539e12fbb4fdcfd262aca
haerxeong <haeryeong04@naver.com>
2024-08-13 18:02:08 +0900

    24.08.13 merge

--- src/domain/ootdLike/ootdLikeController.ts
+++ src/domain/ootdLike/ootdLikeController.ts
@@ -1,36 +1,22 @@
-//src/domain/ootdLike/ootdLikeController.ts
+// src/domain/ootdLike/ootdLikeController.ts
 import {Request, Response, Router} from 'express';
 import {OotdLikeService} from './ootdLikeService';
 import {OotdLikeRequest} from './dto/request';
-import {OotdLikeResponse} from './dto/response';
-import {BaseResponse} from '../../base/baseResponse';
 
 const likeService = new OotdLikeService();
 const router = Router();
 
-router.put('/toggle', async (req: Request, res: Response) => {
+router.get('/check', async (req: Request, res: Response) => {
   try {
-    const {userId, postId}: OotdLikeRequest = req.body;
-    const like = await likeService.toggleLike({userId, postId});
+    const {postId, userId}: {postId: number; userId: number} = req.query as any;
 
-    const response: BaseResponse<OotdLikeResponse | null> = {
-      isSuccess: true,
-      code: 200,
-      message: 'Success',
-      result: like
-        ? {
-            id: like.id,
-            userId: like.userId,
-            postId: like.postId,
-            createdAt: like.createdAt,
-            status: like.status,
-            updatedAt: like.updatedAt,
-            deletedAt: like.deletedAt,
-          }
-        : null,
-    };
+    if (typeof postId !== 'number' || typeof userId !== 'number') {
+      return res.status(400).json({message: 'Invalid request parameters'});
+    }
 
-    res.status(200).json(response);
+    const likeExists = await likeService.checkLike({postId, userId});
+
+    res.status(200).json({exists: likeExists});
   } catch (error: any) {
     res.status(500).json({message: error.message});
   }
