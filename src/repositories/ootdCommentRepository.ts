db0906d12faeb44f5fd4db2334100a18c32258fd
haerxeong <haeryeong04@naver.com>
2024-08-14 01:44:26 +0900

    24.08.14 merge

--- src/repositories/ootdCommentRepository.ts
+++
@@ -1,7 +0,0 @@
-import {Repository} from 'typeorm';
-import {myDataBase} from '../data-source';
-import {Comment} from '../entities/ootdCommentEntity';
-
-export const commentRepository: Repository<Comment> = myDataBase.getRepository(Comment); //그냥 이걸 코드 안에 쓰기
-
-
--- src/repositories/ootdLikeRepository.ts
+++
@@ -1,5 +0,0 @@
-import {Repository} from 'typeorm';
-import {myDataBase} from '../data-source';
-import {Like} from '../entities/ootdLikeEntity';
-
-export const likeRepository: Repository<Like> = myDataBase.getRepository(Like);
