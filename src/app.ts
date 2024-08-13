4b9b598572e562654c7ba488d46f8c9a29b572cb
haerxeong <haeryeong04@naver.com>
2024-08-13 19:51:44 +0900

    24.08.13 merge

--- src/app.ts
+++ src/app.ts
@@ -7,3 +7,24 @@ httpServer.listen(PORT, async () => {
   await startServer();
   console.log(`Server is running on port ${PORT}`);
 });
+=======
+import app from './app.module';
+import * as express from 'express';
+//import { Request, Response } from "express"
+import {myDataBase} from './data-source';
+
+myDataBase
+  .initialize()
+  .then(() => {
+    console.log('DataBase has been initialized!');
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Error during DataBase initialization:', err);
+  });
+
+const PORT = process.env.PORT || 3000;
+>>>>>>> 67918b9 (Feat: 좋아요 누르기/취소 기능 구현)
