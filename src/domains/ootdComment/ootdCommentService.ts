ca6e3162c9122a482385f2fdbff0de57c38fd4cd
haerxeong <haeryeong04@naver.com>
2024-08-13 19:48:35 +0900

    Fix: status 수정

--- src/domains/ootdComment/ootdCommentService.ts
+++ src/domains/ootdComment/ootdCommentService.ts
@@ -2,17 +2,19 @@ import {Repository} from 'typeorm';
 import {Comment} from '../../entities/commentEntity';
 import {CommentRequest} from './dtos/request';
 import {myDataBase} from '../../data-source';
+import dayjs from 'dayjs';
 
 export class CommentService {
   private commentRepository: Repository<Comment> = myDataBase.getRepository(Comment);
 
   async createComment(request: CommentRequest): Promise<Comment> {
     const {userId, postId, content} = request;
-    const now = new Date();
+    const now = dayjs().toDate();
     const newComment = this.commentRepository.create({
       user: {id: userId},
       post: {id: postId},
       content,
+      status: 'activated',
       createdAt: now,
       updatedAt: now,
     });
