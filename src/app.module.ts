ef77f701c1a0da20b72b5f2e769a112f0c39b3b0
Minseo Lim <limms1217@naver.com>
2024-08-20 15:37:19 +0900

    24.08.20 auth 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,6 +18,7 @@ import {initializeDatabase} from './data-source';
 import {authenticateJWT} from './middlewares/authMiddleware';
 import cookieParser from 'cookie-parser';
 import userInterestRouter from './domains/userInterest/userInterestController';
+import userRelationshipRouter from './domains/userRelationship/userRelationshipController';
 
 const chatRoomService = new ChatRoomService();
 const chatMessageService = new ChatMessageService();
@@ -44,7 +45,7 @@ app.use(
 // JWT 인증이 필요한 라우트 (개별적으로 하나씩)
 app.use('/ootd', authenticateJWT, ootdRouter);
 app.use('/chat-rooms', authenticateJWT, chatRoomRouter);
-// app.use('/user-relationships', authenticateJWT, userRelationshipRouter);
+app.use('/user-relationships', authenticateJWT, userRelationshipRouter);
 //app.use("/block", authenticateJWT, blockRouter);
 
 const httpServer = createServer(app);
